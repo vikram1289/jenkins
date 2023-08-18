e7a8c1fea2c33c2495ad34c57fbfd8f9f2045f54
vikram1289 <vikramnani1289@gmail.com>
2023-08-18 14:57:22 +0530

    commit

--- Tyss.Jenkins/src/test/java/JenkinsPractice/CreateUser.java
+++ Tyss.Jenkins/src/test/java/JenkinsPractice/CreateUser.java
@@ -9,13 +9,6 @@ public class CreateUser {
 	{
 		System.out.println("Regression User Created");
 		
-		System.out.println("Changed User Created");
-		
-		System.out.println("Changed User Deleted");
-	
-		System.out.println("Changed User Modified");
-		
-		System.out.println("Changed new User");
 	
 	}
 }
